3d0666b162ee29dd269308357abb0b603a080f3e
Yassir Benjima <Yassirbenjima18@gmail.com>
2024-03-21 22:10:46 +0000

    Setup  Template

--- Bi3Smart/settings.py
+++ Bi3Smart/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIR =[os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
