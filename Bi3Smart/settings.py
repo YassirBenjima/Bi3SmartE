32e39ae5a8f5d59810107c92946b33895250edc5
Yassir Benjima <Yassirbenjima18@gmail.com>
2024-03-22 12:03:18 +0000

    Admin using Jazzmin

--- Bi3Smart/settings.py
+++ Bi3Smart/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +133,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {   
+    'site_header': "Bi3Smart",
+    'site_brand': "Your Order, We deliver",
+    'site_logo' : "/assets/img/logo.png",
+    'copyright' : "Bi3Smart.com",
+}
+
+
