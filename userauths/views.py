21d6b17f02e3570c40065512f7e52df90c9905ed
Yassir Benjima <Yassirbenjima18@gmail.com>
2024-04-03 00:14:39 +0000

    Flash Messages and Alerts

--- userauths/views.py
+++ userauths/views.py
@@ -3,8 +3,8 @@ from userauths.forms import UserRegisterForm
 from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
 from django.conf import settings
+from userauths.models import User
 # Create your views here.
-User = settings.AUTH_USER_MODEL
 def register_view(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST or None)
@@ -35,18 +35,17 @@ def login_view(request):
         password = request.POST.get("password")
         try:
             user = User.objects.get(email=email)
+            user = authenticate(request, email=email , password=password)
+            if user is not None :
+                login(request,user)
+                messages.success(request,f"Hey You are logged in.")
+                return redirect("core:index")
+            else:
+                messages.warning(request,f"User does not exist. Create an account.")
         except:
             messages.warning(request,f"User with {email} does not exist")
-        user = authenticate(request, email=email , password=password)
-        if user is not None :
-            login(request,user)
-            messages.success(request,f"Hey You are logged in.")
-            return redirect("core:index")
-        else:
-            messages.warning(request,f"User does not exist. Create an account.")
-    context = {
-    }
-    return render(request, "userauths/sign-in.html", context)
+   
+    return render(request, "userauths/sign-in.html")
 
 def logout_view(request):
     logout(request)
