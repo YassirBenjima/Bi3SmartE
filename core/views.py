90f718f55bf501f1f3f663f6e4466e77c98ec742
Yassir Benjima <Yassirbenjima18@gmail.com>
2024-04-09 16:50:42 +0000

    Listing Products in Homepage

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from core.models import Product,Category,Vendor,CartOrder,CartOrderItems,ProductImages,ProductReview,Wishlist,Address
 # Create your views here.
 def index(request):
-    return render(request,'core/index.html')
+    # product = Product.objects.all()
+    product = Product.objects.filter(product_status="published" , featured=True)
+    category = Category.objects.all()
+    context = {
+        "products":product,
+        "categories":category,
+    }
+    return render(request,'core/index.html',context)
